7dedd59a342a4dab648e041963a075d789855e9e
Disha27-12 <pate6697@sheridancollege.ca>
2025-05-22 22:46:59 -0500

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,23 +10,50 @@ package card;
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+
+        Random rand = new Random();
+
+        // Fill magic hand with random cards
+        for (int i = 0; i < magicHand.length; i++) {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(rand.nextInt(13) + 1); // 1 to 13
+            c.setSuit(suits[rand.nextInt(suits.length)]); // Random suit
+            magicHand[i] = c;
+
+            // Print card as: Suit Value
+            System.out.println(c.getSuit() + " " + c.getValue());
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a card value (1–13): ");
+        int userValue = input.nextInt();
+
+        System.out.print("Enter a suit (0–3 where 0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades): ");
+        int suitIndex = input.nextInt();
+        String userSuit = suits[suitIndex];
+
+        boolean found = false;
+        for (Card c : magicHand) {
+            if (c.getValue() == userValue && c.getSuit().equalsIgnoreCase(userSuit)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("You found your card in the magic hand! 🎉");
+        } else {
+            System.out.println("Sorry, your card is not in the magic hand.");
+        }
     }
-    
 }
+
